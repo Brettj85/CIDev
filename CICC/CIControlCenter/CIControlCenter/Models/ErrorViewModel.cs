72267d1dee74495bfc78fbfee84476777a470697
Jackson <Brett.Jackson@charter.com>
2019-04-02 10:56:09 -0500

    new MVC template created for steves requested tool

---
+++ CICC/CIControlCenter/CIControlCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CIControlCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
